d18421f1336a4b738634eb6521a5e4600c0b425b
Haein Kim <snowman8775@gmail.com>
2022-04-13 23:09:56 +0900

    바텀 네비게이션 커밋1

---
+++ app/src/main/java/com/example/myapplication/PreferenceFragment.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PreferenceFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.preference_main, container, false)
+        return view
+    }
+}
